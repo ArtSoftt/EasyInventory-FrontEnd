b2b03d5ac034ed171d2490c0815eb6342ccadd2c
Leonardo Taype Fernandez <93153931+Leo2426@users.noreply.github.com>
2023-09-26 12:22:13 -0500

    feat(user): added user component.

---
+++ EasyInventory/src/public/pages/user.component.vue
@@ -0,0 +1,13 @@
+<script setup>
+import toolbarComponent from "@/public/pages/toolbar.component.vue";
+</script>
+
+<template>
+  <header>
+    <toolbar-component></toolbar-component>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
--- EasyInventory/src/router/index.js
+++ EasyInventory/src/router/index.js
@@ -5,6 +5,7 @@ const router= createRouter({
     history: createWebHistory(),
     routes:[
         {path:'/', name:'home', component:()=>import('../public/pages/login.component.vue')},
+        {path:'/user', name:'user', component:()=>import('../public/pages/user.component.vue')},
     ]
 
 })
